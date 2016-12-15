29da9e3dbbe7614916cd7aba9423e7cd29bb22ef
Rey Philip Regis <reyphilipregis@gmail.com>
2016-12-15 16:44:37 +0800

    Chore: Add code to insert data in MongoDB

--- server/main.js
+++ server/main.js
@@ -1,5 +1,21 @@
-import {Meteor} from 'meteor/meteor';
+import _ 		   		from 'lodash';
+import {Meteor}    		from 'meteor/meteor';
+import {Employees} 		from '../imports/collections/employees';
+import {image, helpers} from 'faker';
 
 Meteor.startup(() => {
+	// check if data exists in the collection
+	const numberRecords = Employees.find({}).count();
 
+	if (!numberRecords) {
+		// generate some data
+		_.times(5000, () => {
+			const {name, email, phone} = helpers.createCard();
+
+			Employees.insert({
+				name, email, phone,
+				avatar : image.avatar()
+			});
+		});
+	}
 });
