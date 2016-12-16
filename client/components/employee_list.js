5136f474611d5baa8071a8d42648cc3d4d16f576
Rey Philip Regis <reyphilipregis@gmail.com>
2016-12-16 14:01:17 +0800

    Chore: Improve EmployeeList from functional component to class based component

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,34 +1,46 @@
 // import React
-import React 			   from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
-import { Employees } 	   from '../../imports/collections/employees';
-import EmployeeDetail 	   from './employee_detail';
+import React, { Component } from 'react';
+import { createContainer }  from 'meteor/react-meteor-data';
+import { Employees } 	    from '../../imports/collections/employees';
+import EmployeeDetail 	    from './employee_detail';
 
-// create component
-const EmployeeList = (props) => {
-	// reference props.employees array
-	const { employees } = props;
+const PER_PAGE = 20;
 
-	return (
-		<div>
-			<div className = "employee-list">
-				{ 
-					employees.map((employee) => {
-						return <EmployeeDetail 
-									key = { employee._id } 
-									employee = { employee } />
-					}) 
-				}
+class EmployeeList extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render () {
+		const { employees } = this.props;
+
+		return (
+			<div>
+				<div className = "employee-list">
+					{ 
+						employees.map((employee) => {
+							return <EmployeeDetail 
+										key = { employee._id } 
+										employee = { employee } />
+						}) 
+					}
+				</div>
+
+				<button className="btn btn-primary" onClick = { 
+					() => {
+						Meteor.subscribe('employees', 40)
+					}
+				}>Load More...</button>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 // export the container and pass the EmployeeList to the props of the EmployeeList component
 export default createContainer(() => {
 
 	// set up subscription
-	Meteor.subscribe('employees');
+	Meteor.subscribe('employees', PER_PAGE);
 
 	// return an object. Whatever we return will be sent to EmployeeList as props
 	return { employees: Employees.find({}).fetch() };
--- server/main.js
+++ server/main.js
@@ -22,7 +22,7 @@ Meteor.startup(() => {
 		});
 	}
 
-	Meteor.publish('employees', () => {
-		return Employees.find({}, { limit : 20 });
+	Meteor.publish('employees', (per_page) => {
+		return Employees.find({}, { limit : per_page });
 	});
 });
